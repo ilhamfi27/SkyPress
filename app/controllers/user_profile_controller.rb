795e1d6fd9bba382ef7b84909bad644164306fee
Ilham <ilhamfi_2701@yahoo.com>
2016-08-27 09:30:24 +0700

    User's article lists on user profile

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -2,5 +2,6 @@ class UserProfileController < ApplicationController
 	skip_before_action :authenticate_user!
 	def index
 		@user_profile = User.find(params[:id])
+		@articles = Article.joins(:user).where(author: params[:id]).order('created_at DESC')
 	end
 end
