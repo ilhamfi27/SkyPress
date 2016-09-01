657ac63b109fbffb3f462b1d9512d85a42741c46
Ilham <ilhamfi_2701@yahoo.com>
2016-09-01 09:18:45 +0700

    fix user_profile

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -2,6 +2,6 @@ class UserProfileController < ApplicationController
 	skip_before_action :authenticate_user!
 	def index
 		@user_profile = User.find(params[:id])
-		@articles = Article.joins(:user).where(user_id: current_user.id).order('created_at DESC').page(params[:page]).per(10)
+		@articles = Article.joins(:user).where(user_id: params[:id]).order('created_at DESC').page(params[:page]).per(10)
 	end
 end
